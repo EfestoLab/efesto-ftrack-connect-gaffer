46958d9f0f04ae0530b0668bc484dd11459a1d7d
langeli <lorenzo.angeli@efestolab.uk>
2015-07-24 12:42:24 +0200

    add more debug prints

--- python/GafferFtrackUI/myWidgetPlugValue.py
+++ python/GafferFtrackUI/myWidgetPlugValue.py
@@ -12,7 +12,7 @@ from myWidget import MyWidget
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
+logger.info('loading widget wrapper and plug value')
 
 QtCore = GafferUI._qtImport( "QtCore" )
 QtGui = GafferUI._qtImport( "QtGui" )
@@ -27,12 +27,14 @@ class MyWidgetWrapper(GafferUI.Widget) :
         self.__stateChangedSignal = GafferUI.WidgetSignal()
 
     def stateChangedSignal( self ) :
+        logger.info('stateChangedSignal triggered')
         return self.__stateChangedSignal
 
     def __stateChanged( self, state ) :
         self.__stateChangedSignal( self )
 
     def setState(self, state):
+        logger.info('setting state to : %s ' % state )
         pass
 
     def getState(self):
@@ -52,6 +54,8 @@ class MyWidgetPlugValue(GafferUI.PlugValueWidget):
         self._updateFromPlug()
 
     def _updateFromPlug( self ) :
+        logger.info('updating from plug...')
+
         if self.getPlug() is not None :
             with self.getContext() :
                 with Gaffer.BlockedConnection( self.__stateChangedConnection ) :
@@ -60,10 +64,14 @@ class MyWidgetPlugValue(GafferUI.PlugValueWidget):
         self.__myWidget.setEnabled( self._editable() )
 
     def setHighlighted( self, highlighted ) :
+        logger.info('setting as highlited')
+
         GafferUI.PlugValueWidget.setHighlighted( self, highlighted )
         self.__myWidget.setHighlighted( highlighted )
 
     def __stateChanged( self, widget ) :
+        logger.info('setting state changed')
+
         self.__setPlugValue()
         return False
 
