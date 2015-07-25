71ba62aa4e84a1416e0c3f8408008042edadeb7d
langeli <lorenzo.angeli@efestolab.uk>
2015-07-25 20:50:34 +0200

    remove unnecessary code for easier debug

--- python/GafferFtrackUI/myWidgetPlugValue.py
+++ python/GafferFtrackUI/myWidgetPlugValue.py
@@ -25,22 +25,6 @@ class MyWidgetWrapper(GafferUI.Widget) :
         logger.info('creating: %s ' % self.__class__.__name__)
         self.mywidget = MyWidget()
         super(MyWidgetWrapper, self).__init__(self.mywidget, *args, **kw)
-        self.__stateChangedSignal = GafferUI.WidgetSignal()
-
-    def stateChangedSignal( self ) :
-        logger.info('stateChangedSignal triggered')
-        return self.__stateChangedSignal
-
-    def __stateChanged( self, state ) :
-        self.__stateChangedSignal( self )
-
-    def setState(self, state):
-        logger.info('setting state to : %s ' % state )
-        pass
-
-    def getState(self):
-        logger.info('getting state')
-        return True
 
 
 class MyWidgetPlugValue(GafferUI.PlugValueWidget):
@@ -50,37 +34,10 @@ class MyWidgetPlugValue(GafferUI.PlugValueWidget):
         logger.info('creating: %s ' % self.__class__.__name__)
 
         self.__myWidget = MyWidgetWrapper()
-        super(MyWidgetPlugValue, self).__init__(self.__myWidget, *args, **kw )
+        super(MyWidgetPlugValue, self).__init__(self.__myWidget, *args, **kw)
         self._addPopupMenu(self.__myWidget)
-        self.__stateChangedConnection = self.__myWidget.stateChangedSignal().connect(
-            Gaffer.WeakMethod(self.__stateChanged)
-        )
         self._updateFromPlug()
 
     def _updateFromPlug( self ) :
         logger.info('updating from plug...')
-
-        if self.getPlug() is not None :
-            with self.getContext() :
-                with Gaffer.BlockedConnection( self.__stateChangedConnection ) :
-                    self.__myWidget.setState( self.getPlug().getValue() )
-
-        self.__myWidget.setEnabled( self._editable() )
-
-    def setHighlighted( self, highlighted ) :
-        logger.info('setting as highlited')
-
-        GafferUI.PlugValueWidget.setHighlighted( self, highlighted )
-        self.__myWidget.setHighlighted( highlighted )
-
-    def __stateChanged( self, widget ) :
-        logger.info('setting state changed')
-
-        self.__setPlugValue()
-        return False
-
-    def __setPlugValue( self ) :
-        logger.info('setting plug value')
-
-        with Gaffer.UndoContext( self.getPlug().ancestor( Gaffer.ScriptNode ) ) :
-            self.getPlug().setValue( self.__myWidget.getState() )
+        pass
