20ae6e283aaabb60ed9636f09d24e702c06d9050
langeli <lorenzo.angeli@efestolab.uk>
2015-07-27 12:16:28 +0200

    create Import inheriting from base Gaffer.Node (https://gist.github.com/johnhaddon/78b5b646f65f462c66d9)

--- python/GafferFtrack/FtrackImport.py
+++ python/GafferFtrack/FtrackImport.py
@@ -8,13 +8,17 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class FtrackImport(GafferScene.AlembicSource):
+class FtrackImport(Gaffer.Node):
     def __init__(self, name='FtrackImport'):
         super(FtrackImport, self).__init__(name=name)
-        logger.info('creating : %s' % self.__class__.__name__)
-        logger.info(self['fileName'])
-        # add manually a chid
-        self.addChild(Gaffer.StringPlug('something'))
 
+        Gaffer.Node.__init__( self, name )
 
-IECore.registerRunTimeTyped(FtrackImport, typeName="Ftrack::Import" )
+        self["fileName"] = Gaffer.StringPlug()
+        self["out"] = GafferScene.ScenePlug( direction = Gaffer.Plug.Direction.Out )
+
+        self["__reader"] = GafferScene.AlembicSource()
+        self["__reader"]["fileName"].setInput( self["fileName"] )
+        self["out"].setInput( self["__reader"]["out"] )
+
+IECore.registerRunTimeTyped(FtrackImport)
