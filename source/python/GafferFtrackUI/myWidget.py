da4dbc6e0aa9dd0063156644f123c016e55ed8e3
langeli <lorenzo.angeli@efestolab.uk>
2015-07-27 15:34:22 +0200

    expand widget, test signals

--- source/python/GafferFtrackUI/myWidget.py
+++ source/python/GafferFtrackUI/myWidget.py
@@ -2,25 +2,49 @@ import sys
 import logging
 
 from PySide import QtGui, QtCore
+import ftrack
+
+try:
+    ftrack.setup()
+except:
+    pass
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+class AssetPicker(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(AssetPicker, self).__init__(parent=parent)
+        self.layout = QtGui.QVBoxLayout()
+        btn = QtGui.QPushButton('HERE WE GO')
+        self.layout.addWidget(btn)
+        self.setLayout(self.layout)
 
 
 class MyWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         logger.info('creating : %s' % self.__class__.__name__)
         super(MyWidget, self).__init__(parent=parent)
-        layout = QtGui.QVBoxLayout()
-        hlayout = QtGui.QHBoxLayout()
-        layout.addLayout(hlayout)
-        versions = QtGui.QComboBox()
-        self.setLayout(layout)
-        path = QtGui.QLineEdit()
-        btn = QtGui.QPushButton('select asset')
-        hlayout.addWidget(path)
-        hlayout.addWidget(btn)
-        layout.addWidget(versions)
+        self.asset_picker = AssetPicker()
+        self.build_ui()
+        self.connect_signals()
+
+    def build_ui(self):
+        self.mainlayout = QtGui.QVBoxLayout()
+        self.hlayout = QtGui.QHBoxLayout()
+        self.mainlayout.addLayout(self.hlayout)
+        self.versions = QtGui.QComboBox()
+        self.setLayout(self.mainlayout)
+        self.asset_path = QtGui.QLineEdit()
+        self.asset_btn = QtGui.QPushButton('select asset')
+        self.hlayout.addWidget(self.asset_path)
+        self.hlayout.addWidget(self.asset_btn)
+        self.mainlayout.addWidget(self.versions)
+
+    def connect_signals(self):
+        self.asset_btn.clicked.connect(self.asset_picker.exec_)
+
 
 if __name__ == '__main__':
 
